d2ad29c8a0295967cb3df3d801a27878f1840b1c
jebovic <jba@amiralweb.com>
2014-04-15 18:09:25 +0200

    Skin : first try

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,17 +3,16 @@
 /* Controllers */
 
 angular.module('readerApp.controllers', [])
-    .controller('RandomController', ['$scope', 'Storie', 'Site', '$sce', function ($scope, Storie, Site, $sce) {
+    .controller('RandomController', ['$scope', 'Storie', 'Site', '$sce', 'cacheService', function ($scope, Storie, Site, $sce, cacheService) {
         $scope.loading = true;
         $scope.limit = 10;
         $scope.stories = [];
-        $scope.limitButtons = {1: "Une à la fois", 10: "10 d'un coup", 25: "25 massif !", 50: "50 grosse caisse !"};
+        $scope.limitButtons = {1: "1", 10: "10", 25: "25", 50: "50"};
         $scope.sites = [];
         $scope.activeSites = [];
         $scope.templates = {
             'header': { url: 'partials/header.html'},
-            'menuItems': { url: 'partials/menu/items.html'},
-            'menuSites': { url: 'partials/menu/sites.html'},
+            'settings': { url: 'partials/menu/settings.html'},
             'storyListItem': {url: 'partials/story/list-item.html'}
         };
         Site.query({}, function (response) {
@@ -48,16 +47,28 @@ angular.module('readerApp.controllers', [])
             $scope.stories = [];
             $scope.loadStories();
         };
+        $scope.isRead = function( bool ) {
+            return bool ? 'fa-square' : 'fa-square-o';
+        };
         $scope.loadStories = function () {
             var siteIds = [];
             angular.forEach($scope.activeSites, function(value, key){
                 siteIds.push(value.id);
             });
+            var cachedStories = cacheService.getData('readStories');
+            var readStories = cachedStories ? angular.fromJson(cachedStories) : [];
             Storie.random({limit: $scope.limit, sites: siteIds.join(',')}, function (response) {
                 angular.forEach(response.stories, function (story, key) {
                     story.toAnimate = false;
+                    story.unread = true;
+                    var storyIndex = readStories.indexOf(story.id);
+                    if (storyIndex != -1) {
+                        story.unread = false;
+                    }
                     $scope.stories.push(story);
+                    readStories.push(story.id);
                 });
+                cacheService.setData('readStories', readStories);
                 $scope.loading = false;
             });
         };
--- app/js/services.js
+++ app/js/services.js
@@ -5,17 +5,46 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('readerApp.services', ['ngResource']).
-  value('version', '0.1')
-  .factory('Storie', ['$resource',
-    function($resource){
-      return $resource('http://api.reader.loc/stories', {limit:'@limit', sites: '@sites'}, {
-        query: {method:'GET', params:{limit:10, sites:[]}},
-        random: {url:'http://api.reader.loc/stories/random', method:'GET',params:{limit:10, sites:[]}}
-      });
-    }])
-    .factory('Site', ['$resource',
-        function($resource){
-            return $resource('http://api.reader.loc/sites', {}, {
-                query: {method:'GET', params:{}}
+    value('version', '0.1')
+    .factory('Storie', ['$resource',
+        function ($resource) {
+            return $resource('http://api.reader.loc/stories', {limit: '@limit', sites: '@sites'}, {
+                query: {method: 'GET', params: {limit: 10, sites: []}},
+                random: {url: 'http://api.reader.loc/stories/random', method: 'GET', params: {limit: 10, sites: []}}
             });
-        }]);
+        }])
+    .factory('Site', ['$resource',
+        function ($resource) {
+            return $resource('http://api.reader.loc/sites', {}, {
+                query: {method: 'GET', params: {}}
+            });
+        }])
+    .factory('storageService', function () {
+        return {
+            get: function (key) {
+                return localStorage.getItem(key);
+            },
+            save: function (key, data) {
+                localStorage.setItem(key, JSON.stringify(data));
+            },
+            remove: function (key) {
+                localStorage.removeItem(key);
+            },
+            clearAll: function () {
+                localStorage.clear();
+            }
+        };
+    })
+    .factory('cacheService',['storageService', function (storageService) {
+        return {
+            getData: function (key) {
+                return storageService.get(key);
+            },
+            setData: function (key, data) {
+                storageService.save(key, data);
+            },
+            removeData: function (key) {
+                storageService.remove(key);
+            }
+        };
+    }]);
